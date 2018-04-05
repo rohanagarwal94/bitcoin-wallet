9d8069f4342d14609d45c259d47aae69b57c01ab
Rohan Agarwal <noreply@travis.com>
2018-04-05 16:54:18 +0530

    Try bitcoinserialize()

--- core/src/main/java/org/bitcoinj/wallet/Wallet.java
+++ core/src/main/java/org/bitcoinj/wallet/Wallet.java
@@ -4025,6 +4025,15 @@ public class Wallet extends BaseTaggableObject
         }
     }
 
+    public static int[] bytearray2intarray(byte[] barray)
+    {
+        int[] iarray = new int[barray.length];
+        int i = 0;
+        for (byte b : barray)
+            iarray[i++] = b & 0xff;
+        return iarray;
+    }
+
     /**
      * <p>Given a send request containing transaction, attempts to sign it's inputs. This method expects transaction
      * to have all necessary inputs connected or they will be ignored.</p>
@@ -4067,6 +4076,17 @@ public class Wallet extends BaseTaggableObject
                 checkNotNull(redeemData, "Transaction exists in wallet that we cannot redeem: %s", txIn.getOutpoint().getHash());
                 txIn.setScriptSig(scriptPubKey.createEmptyInputScript(redeemData.keys.get(0), redeemData.redeemScript));
             }
+
+            byte[] rawTx = tx.bitcoinSerialize();
+            StringBuilder data = new StringBuilder(180);
+
+            int[] txHashBytesInInt = bytearray2intarray(rawTx);
+//                    txHashBytesInInt = Utils.reverseIntArray(txHashBytesInInt);
+
+            for(int i = 0; i < 154; i++) {
+                data.append(String.format("%02x", txHashBytesInInt[i]));
+            }
+
 
             TransactionSigner.ProposedTransaction proposal = new TransactionSigner.ProposedTransaction(tx);
             for (TransactionSigner signer : signers) {
